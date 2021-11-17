58f9265ad042db7c20bdcf43dba5eff3e40b7076
unknown <ttraqeeb@gmail.com>
2021-11-17 12:49:31 +0600

    project upload

---
+++ practice 2/third_project/login_app/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from django.urls import path
+from login_app import views
+
+#for image show
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+app_name = 'login_app'
+
+urlpatterns = [
+path('', views.index, name = 'index'),
+path('register/', views.register, name = 'register'),
+path('login/', views.login_page, name ='login'),
+path('user_login/', views.user_login, name = 'user_login'),
+path('logout/', views.user_logout, name = 'logout'),
+]
+# for image
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
